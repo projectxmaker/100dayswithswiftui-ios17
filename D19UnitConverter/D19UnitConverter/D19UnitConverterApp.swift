5da5fe8ed288e22c257f047df662818c15d0d74f
Tuan <projectxmaker@gmail.com>
2023-10-16 06:55:29 +0700

    D19 - Unit Converter: Init code

---
+++ D19UnitConverter/D19UnitConverter/D19UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  D19UnitConverterApp.swift
+//  D19UnitConverter
+//
+//  Created by Pham Anh Tuan on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct D19UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

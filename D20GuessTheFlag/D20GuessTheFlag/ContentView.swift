3cf66115de61d22b7d08b137bf674d5b5d799f3b
Tuan <projectxmaker@gmail.com>
2023-10-16 15:11:50 +0700

    D20 - Guess The Flag: Init code

---
+++ D20GuessTheFlag/D20GuessTheFlag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  D20GuessTheFlag
+//
+//  Created by Pham Anh Tuan on 10/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

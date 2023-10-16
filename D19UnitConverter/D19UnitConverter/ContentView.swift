af1803e42f2780b04a4b3dff327d78a480281a88
Tuan <projectxmaker@gmail.com>
2023-10-16 11:35:48 +0700

    D19 - Unit Converter: code of working app

--- D19UnitConverter/D19UnitConverter/ContentView.swift
+++ D19UnitConverter/D19UnitConverter/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputValue = 0.0
+    @State private var inputUnit = "Feet"
+    @State private var outputUnit = "Feet"
+    
+    private let units = [
+        "Feet": UnitLength.feet,
+        "Inches": UnitLength.inches,
+        "Meters": UnitLength.meters,
+        "Kilometers": UnitLength.kilometers,
+        "Centimeters": UnitLength.centimeters,
+        "Deimeters": UnitLength.decimeters,
+        "Millimeters": UnitLength.millimeters
+    ]
+    
+    private var outputValue: String {
+        let convertedInputValue = Measurement(value: inputValue, unit: units[inputUnit] ?? UnitLength.feet)
+        let convertedOutputValue = convertedInputValue.converted(to: units[outputUnit] ?? UnitLength.feet)
+        
+        return convertedOutputValue.description
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Input") {
+                    TextField("Value", value: $inputValue, format: .number)
+                    Picker("Unit", selection: $inputUnit) {
+                        ForEach(Array(units.keys), id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("Output") {
+                    Picker("Unit", selection: $outputUnit) {
+                        ForEach(Array(units.keys), id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                    
+                    Text(outputValue)
+                }
+            }
+            .navigationTitle("Unit Converter")
         }
-        .padding()
     }
 }
 
